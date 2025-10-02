2f593dfe0ff1e2dba85761025ea0f3f78f9c6393
Kadir-Hali <kadirhali07@gmail.com>
2025-10-02 21:58:39 +0300

    2.10.25-1

--- DemoNightStore/Hubs/StoreHub.cs
+++ DemoNightStore/Hubs/StoreHub.cs
@@ -1,5 +1,6 @@
 ﻿using DemoNightStore.Context;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 namespace DemoNightStore.Hubs
 {
@@ -12,6 +13,19 @@ namespace DemoNightStore.Hubs
             _context = context;
         }
 
+        public async Task GetOrders()
+        {
+            var values = await _context.Orders.ToListAsync();
+            await Clients.Caller.SendAsync("ReceiveOrders", values);
+        }
 
+        public async Task Statistics()
+        {
+            var orderCount = await _context.Orders.CountAsync();
+            await Clients.Caller.SendAsync("ReceiveOrderCount", orderCount);
+
+            var orderCountByPrice = await _context.Orders.Where(x => x.Price > 1000).CountAsync();
+            await Clients.Caller.SendAsync("ReceiveOrderCountByPrice", orderCountByPrice);
+        }
     }
 }
--- DemoNightStore/Program.cs
+++ DemoNightStore/Program.cs
@@ -1,6 +1,13 @@
+using DemoNightStore.Context;
+using DemoNightStore.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<StoreContext>();
+builder.Services.AddSignalR();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -12,6 +19,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.MapHub<StoreHub>("/storeHub");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
